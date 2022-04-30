3b045e8a6360f743890591c6b6645a5bf3dc3ab8
YONGJU <odetsuo@gmail.com>
2022-04-30 15:43:54 +0900

    fix: Dollar 부작용 해결 (불변객체)

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 - [ ] $5 + 1-CHF = $10
 - [x] $5 X 2 = $10
 - [ ] amount 를 private 으로 만들기 
-- [ ] Dollar 부작용
+- [x] Dollar 부작용
 - [ ] Money 반올림
--- src/test/java/com/example/tddbyexample/DollarTest.java
+++ src/test/java/com/example/tddbyexample/DollarTest.java
@@ -12,9 +12,9 @@ class DollarTest {
     void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertThat(five.amount).isEqualTo(10);
+        assertThat(product.amount).isEqualTo(10);
         product = five.times(3);
-        assertThat(five.amount).isEqualTo(15);
+        assertThat(product.amount).isEqualTo(15);
     }
 
 }
