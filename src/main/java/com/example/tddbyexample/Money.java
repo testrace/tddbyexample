bb6cf8f1bb821eb855e99b7f00e4b63b386c012b
YONGJU <odetsuo@gmail.com>
2022-04-30 16:52:51 +0900

    fix: 금액과 클래스가 서로 동일할 때만 서로 같은 것으로 판단

--- src/main/java/com/example/tddbyexample/Money.java
+++ src/main/java/com/example/tddbyexample/Money.java
@@ -1,10 +1,12 @@
 package com.example.tddbyexample;
 
 public class Money {
+
     protected int amount;
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount
+            && getClass().equals(money.getClass());
     }
 }
