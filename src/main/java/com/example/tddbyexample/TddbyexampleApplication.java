b1ae5e8fb00f66716c215fb62a1e85180b8c94b0
YONGJU <odetsuo@gmail.com>
2022-04-30 14:56:25 +0900

    first commit

---
+++ src/main/java/com/example/tddbyexample/TddbyexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddbyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddbyexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddbyexampleApplication.class, args);
+    }
+
+}
