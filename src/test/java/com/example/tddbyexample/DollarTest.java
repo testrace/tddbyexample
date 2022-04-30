4359d0448ca14df5b8145b30950cd9492789338e
YONGJU <odetsuo@gmail.com>
2022-04-30 17:05:38 +0900

    refactor test: Dollar 직접 참조 제거
    직접 참조를 제거하면 어떠한 클라이언트 코드도 Dollar 라는 이름의 하위 클래스가 있다는 사실을 알지 못한다.
    하위 클래스의 존재를 테스트에서 분리함으로써 어떤 모델 코드에도 영향을 주지 않고 상속 구조를
    마음대로 변경할 수 있게 되었다.

--- src/test/java/com/example/tddbyexample/DollarTest.java
+++ src/test/java/com/example/tddbyexample/DollarTest.java
@@ -11,17 +11,17 @@ class DollarTest {
     @Test
     void testMultiplication() {
         Money five = Money.dollar(5);
-        assertThat(five.times(2)).isEqualTo(new Dollar(10));
-        assertThat(five.times(3)).isEqualTo(new Dollar(15));
+        assertThat(five.times(2)).isEqualTo(Money.dollar(10));
+        assertThat(five.times(3)).isEqualTo(Money.dollar(15));
     }
 
     @Test
     void testEquality() {
-        assertThat(new Dollar(5).equals(new Dollar(5))).isTrue();
-        assertThat(new Dollar(5).equals(new Dollar(6))).isFalse();
+        assertThat(Money.dollar(5).equals(Money.dollar(5))).isTrue();
+        assertThat(Money.dollar(5).equals(Money.dollar(6))).isFalse();
         assertThat(new Franc(5).equals(new Franc(5))).isTrue();
         assertThat(new Franc(5).equals(new Franc(6))).isFalse();
-        assertThat(new Franc(5).equals(new Dollar(6))).isFalse();
+        assertThat(new Franc(5).equals(Money.dollar(6))).isFalse();
     }
 
     @Test
