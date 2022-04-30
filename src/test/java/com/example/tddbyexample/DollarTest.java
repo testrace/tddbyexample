daca8e6c96e1203d0019ac61d6f98688e7cbdd8b
YONGJU <odetsuo@gmail.com>
2022-04-30 15:13:48 +0900

    test: 실패하는 테스트 작성

--- src/test/java/com/example/tddbyexample/DollarTest.java
+++ src/test/java/com/example/tddbyexample/DollarTest.java
@@ -1,2 +1,16 @@
-package com.example.tddbyexample;public class DollarTest {
+package com.example.tddbyexample;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DollarTest {
+
+    @DisplayName("$5 X 2 = $10")
+    @Test
+    void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertThat(five.amount).isEqualTo(10);
+    }
+    
 }
