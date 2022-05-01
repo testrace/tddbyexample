e83eb4ee7fe132d37cb18ee75b0e14d3986665e2
YONGJU <odetsuo@gmail.com>
2022-05-01 13:47:16 +0900

    feat: 통화 추상 메서드 추가

--- src/main/java/com/example/tddbyexample/Money.java
+++ src/main/java/com/example/tddbyexample/Money.java
@@ -14,9 +14,12 @@ public abstract class Money {
 
     abstract Money times(int multiplier);
 
+    abstract String currency();
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
             && getClass().equals(money.getClass());
     }
+
 }
