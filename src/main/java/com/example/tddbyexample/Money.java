7a6e08a8e1752994e7e26f5f12e4fb19b7cb7d01
YONGJU <odetsuo@gmail.com>
2022-05-01 14:20:36 +0900

    refactor: times를 상위 클래스로 올리기 위해 정적 팩토리 메서드에서 생성자로 변경
    testFrancMultiplication 테스트가 실패한다.

--- src/main/java/com/example/tddbyexample/Money.java
+++ src/main/java/com/example/tddbyexample/Money.java
@@ -1,6 +1,6 @@
 package com.example.tddbyexample;
 
-public abstract class Money {
+public class Money {
 
     protected int amount;
     protected String currency;
@@ -18,7 +18,9 @@ public abstract class Money {
         return new Franc(amount, "CHF");
     }
 
-    abstract Money times(int multiplier);
+    Money times(int multiplier) {
+        return null;
+    }
 
     String currency() {
         return currency;
