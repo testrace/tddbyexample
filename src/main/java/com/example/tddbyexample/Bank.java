b4ec9ed1ca5df24cd6a9577729f67ab9ea5b0af2
YONGJU <odetsuo@gmail.com>
2022-05-01 17:13:26 +0900

    refactor: 환율 계산 로직을 Bank로 이동

--- src/main/java/com/example/tddbyexample/Bank.java
+++ src/main/java/com/example/tddbyexample/Bank.java
@@ -6,4 +6,7 @@ public class Bank {
         return source.reduce(this, to);
     }
 
+    public int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+    }
 }
