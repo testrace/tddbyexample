3f9ee6aff21bc8c44edc509e9df79ba1f08cdadb
YONGJU <odetsuo@gmail.com>
2022-05-01 16:32:37 +0900

    feat: Bank#reduce 구현

--- src/main/java/com/example/tddbyexample/Bank.java
+++ src/main/java/com/example/tddbyexample/Bank.java
@@ -3,6 +3,8 @@ package com.example.tddbyexample;
 public class Bank {
 
     public Money reduce(Expression source, String to) {
-        return Money.dollar(10);
+        Sum sum = (Sum) source;
+        int amount = sum.augend.amount + sum.addend.amount;
+        return new Money(amount, to);
     }
 }
