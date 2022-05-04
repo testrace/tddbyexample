f99eb7448f6c815a008c91a0cb622083686d1307
YONGJU <odetsuo@gmail.com>
2022-05-04 18:15:19 +0900

    refactor: 환율을 저장하기 위한 자료구조 추가

--- src/main/java/com/example/tddbyexample/Bank.java
+++ src/main/java/com/example/tddbyexample/Bank.java
@@ -1,6 +1,10 @@
 package com.example.tddbyexample;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
 
     public Money reduce(Expression source, String to) {
         return source.reduce(this, to);
@@ -9,4 +13,5 @@ public class Bank {
     public int rate(String from, String to) {
         return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
     }
+
 }
