dc59454f9fd900762a03d1787315501be1d39243
YONGJU <odetsuo@gmail.com>
2022-05-05 09:07:17 +0900

    refactor: 반환 및 인자 타입을 상위 타입으로 변경

--- src/test/java/com/example/tddbyexample/MoneyTest.java
+++ src/test/java/com/example/tddbyexample/MoneyTest.java
@@ -78,7 +78,7 @@ class MoneyTest {
     @Test
     void testMixedAddition() {
         Money fiveBucks = Money.dollar(5);
-        Money tenFrancs = Money.franc(10);
+        Expression tenFrancs = Money.franc(10);
         Bank bank = new Bank();
         bank.addRate("CHF", "USD", 2);
         Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
